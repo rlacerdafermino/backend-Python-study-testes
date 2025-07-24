8d1bc468b2c508820cb42971d95bad0d01c74853
Rafael Lacerda Fermino <rlacerdafermino@outlook.com>
2025-07-24 12:39:49 -0300

    Lista iniciada em Python

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("Hello World")
+a = [1,2,3,4,5,6]
+
+
+def add_number ():
+    numero = int(input("Digite um número: "))
+    a.append(numero)
+    print(f"Número {numero} adicionado à lista a.")
+
+def get_list():
+    print("Lista atual:", a)
+
+def apresentacao():
+    print("Bem-vindo ao programa de manipulação de lista!")
+    print("Você pode adicionar números à lista ou visualizar a lista atual.")
+
+
+__init__ = {
+     apresentacao(),
+}
